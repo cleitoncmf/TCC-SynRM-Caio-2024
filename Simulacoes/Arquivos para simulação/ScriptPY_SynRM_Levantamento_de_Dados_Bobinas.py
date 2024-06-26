06ad8f7a89af1b8ef5d4400306cbdb355149cb68
Caio-Alexandre <83480764+Caio-Alexandre@users.noreply.github.com>
2024-06-26 20:37:24 -0300

    Dados do Projeto

---
+++ Simulacoes/Arquivos para simulação/ScriptPY_SynRM_Levantamento_de_Dados_Bobinas.py
@@ -0,0 +1,119 @@
+#Script para girar o rotor da máquina, ler o fluxo concatenado pelas bobinas e salvar os resultados em um arquivo .csv
+#Biblioteca necessária pyFEMM, baixe no cmd com 'pip install pyfemm'
+import csv
+import femm
+
+nome_do_arquivo = 'Protótipo de rotor de relutância_4polos_M-36.fem'
+nome_tabela_ref = "Corrente_8A_delta25.csv"
+
+femm.openfemm()
+femm.opendocument(nome_do_arquivo)
+
+#Função para iterar sobre uma coluna, encontrar um valor específico 
+#e retornar uma dado de outra coluna na mesma linha
+def buscar_valor(csv_file, coluna_busca, valor_busca, coluna_retorno):
+    with open(csv_file, 'r') as arquivo_csv:
+        leitor_csv = csv.reader(arquivo_csv)
+        
+        # Itera sobre as linhas do arquivo CSV
+        for linha in leitor_csv:
+            # Verifica se o valor de coluna_busca corresponde ao valor buscado
+            if linha[coluna_busca] == valor_busca:
+                # Retorna o valor associado à coluna_retorno
+                return linha[coluna_retorno]
+    
+    # Se o valor não for encontrado, retorna None
+    return None
+
+#Cria ou abre o arquivo onde serão gavados os valores dos fluxos concatenados
+f = 'Dados_M-36_Trifásico.csv'
+cabecalho = ["Posicao","Fluxo_Concatenado_estatorA","Fluxo_Concatenado_estatorB",
+             "Fluxo_Concatenado_estatorC","Tensao_A","Tensao_B","Tensao_C",
+             "Indutancia_A","Indutancia_B","Indutancia_C","R_A","R_B","R_C",
+             "Potencia_A","Potencia_B","Potencia_C"]
+
+# Abre o arquivo CSV no modo de escrita
+with open(f, 'w', newline='') as arquivo_csv:
+    # Cria um objeto escritor CSV
+    escritor_csv = csv.writer(arquivo_csv)
+    
+    # Escreve o cabeçalho no arquivo CSV
+    escritor_csv.writerow(cabecalho)
+    print(f'O arquivo CSV "{f}" foi criado com sucesso.\n')
+
+#Intervalo entre os ângulos e contador de iterações do loop
+#delta_angulo = int(input("Intervalo dos angulos: "))
+delta_angulo = 5
+cont=0
+
+#Se for coletar dados para corrente continua, especificar as correntes abaixo
+#Para funcionar, comente os modificadores de corrente dentro do for loop
+femm.mi_modifycircprop("BobinaEstatorA",1,0)
+femm.mi_modifycircprop("BobinaEstatorB",1,0)
+femm.mi_modifycircprop("BobinaEstatorC",1,0)
+
+#Repetição para girar o rotor e computar os resultados
+for angulo in range(0,366-delta_angulo,delta_angulo):
+    #'Setando' os valores de corrente de acordo com o CSV de referencia, as variáveis dos circuitos são interpretadas em strings
+    femm.mi_modifycircprop("BobinaEstatorA",1,
+                           buscar_valor(nome_tabela_ref,4,str(angulo),1))
+    femm.mi_modifycircprop("BobinaEstatorB",1,
+                           buscar_valor(nome_tabela_ref,4,str(angulo),2))
+    femm.mi_modifycircprop("BobinaEstatorC",1,
+                           buscar_valor(nome_tabela_ref,4,str(angulo),3)) 
+
+    #O contorno do rotor, assim como os materiais estão no grupo 5
+    femm.mi_selectgroup(5)
+
+    #Roda a analise de resultados
+    femm.mi_analyze()
+
+    #Mostra o resultado da simulacao
+    femm.mi_loadsolution()
+    
+    #Mostra a densidade de fluxo
+    #Se habilitou a apresentação da solução e quiser mostar a densidade de fluxo
+    #Fica muito mais lento
+    #femm.mo_showdensityplot(0,0,1.6,10**-8,"bmag")
+    
+    #Salva o print do resultado (posição inicial)
+    #Se quiser salvar um print de cada imagem, descomente
+    #Fica mais lento
+    femm.mo_savebitmap(f'figuras\posicao_{angulo}_graus.bmp')
+    
+    #Computa as variáveis das bobinas do Estator
+    Corrente_estatorA,Tensao_estatorA,Fluxo_Concatenado_estatorA = femm.mo_getcircuitproperties("BobinaEstatorA")
+    Corrente_estatorB,Tensao_estatorB,Fluxo_Concatenado_estatorB = femm.mo_getcircuitproperties("BobinaEstatorB")
+    Corrente_estatorC,Tensao_estatorC,Fluxo_Concatenado_estatorC = femm.mo_getcircuitproperties("BobinaEstatorC")
+
+    #Grava os resultados de interesse na lista resultado. Se qualquer das correntes for zero o programa irá dar erro
+    #Personalize a lista resultado abaixo de acordo
+    resultado = [angulo,Fluxo_Concatenado_estatorA,Fluxo_Concatenado_estatorB,
+                 Fluxo_Concatenado_estatorC,Tensao_estatorA,Tensao_estatorB,
+                 Tensao_estatorC,Fluxo_Concatenado_estatorA/Corrente_estatorA,
+                 Fluxo_Concatenado_estatorB/Corrente_estatorB,
+                 Fluxo_Concatenado_estatorC/Corrente_estatorC,
+                 Tensao_estatorA/Corrente_estatorA,
+                 Tensao_estatorB/Corrente_estatorB,
+                 Tensao_estatorC/Corrente_estatorC,
+                 Tensao_estatorA*Corrente_estatorA,
+                 Tensao_estatorB*Corrente_estatorB,
+                 Tensao_estatorC*Corrente_estatorC]
+    with open(f,'a', newline='') as arquivo_csv:
+        #Cria um objeto escritor CSV
+        escritor_csv = csv.writer(arquivo_csv)
+    
+        #Escreve o cabeçalho no arquivo CSV
+        escritor_csv.writerow(resultado)
+    #Gira o rotor ao redor do ponto (0,0)
+    femm.mi_selectgroup(5)
+    femm.mi_moverotate(0,0,-delta_angulo)
+    
+    cont+=1
+    print(f"Iteração {cont} de {round(360/delta_angulo)}.")
+    print("Corrente A: ",Corrente_estatorA,"\nCorrente B: ",Corrente_estatorB,"\nCorrente C: ",Corrente_estatorC,"\n")
+
+#Alinhar o Rotor
+femm.mi_selectgroup(5)
+femm.mi_moverotate(0,0,-(360-angulo))
+print("Concluído")
--- Simulacoes/README.md
+++ Simulacoes/README.md
@@ -1,3 +1,61 @@
 # Simulações
 
-Coloca nesta pasta as simulações (todas que voce usou) e descreve neste arquivo readme qual a diferença de cada uma
+## Arquivos para simulação
+
+Se tiver a intenção de testar o script, a pasta contém o arquivo FEM requerido, os dados da corrente em formato CSV e o script para levantar dados gerais da máquina.
+(Obs: A tensão induzida é apenas resistiva e não deve ser considerada, o mesmo para a potência)
+
+## Curvas de magnetização da máquina
+
+Essa pasta contém a curva *BxH* do Aço M-36 (Curva B-H M-36.csv) e as curvas de fluxo de eixo direto e em quadratura na bobina *A* em função da corrente *Ia*.
+Caso queira replicar a medição dos fluxos dq, comente os modificadores de circuito dentro do loop e coloque a corrente desejada acima do loop (No exemplo abaixo, o motor irá girar com a corrente de 8A na bobina A somente).
+
+
+```
+#Se for coletar dados para corrente continua, especificar as correntes abaixo
+#Para funcionar, comente os modificadores de corrente dentro do for loop
+femm.mi_modifycircprop("BobinaEstatorA",1,8)
+femm.mi_modifycircprop("BobinaEstatorB",1,0)
+femm.mi_modifycircprop("BobinaEstatorC",1,0)
+
+#Repetição para girar o rotor e computar os resultados
+for angulo in range(0,366-delta_angulo,delta_angulo):
+    #'Setando' os valores de corrente de acordo com o CSV de referencia, as variáveis dos circuitos são interpretadas em strings
+    #femm.mi_modifycircprop("BobinaEstatorA",1,
+                           buscar_valor(nome_tabela_ref,4,str(angulo),1))
+    #femm.mi_modifycircprop("BobinaEstatorB",1,
+                           buscar_valor(nome_tabela_ref,4,str(angulo),2))
+    #femm.mi_modifycircprop("BobinaEstatorC",1,
+                           buscar_valor(nome_tabela_ref,4,str(angulo),3)) 
+                           
+```
+
+## Levantamento das Indutâncias da Máquina
+
+Essa pasta possui as curvas de fluxo concatenado na Bobina A para corrente de 5A na Bobina A.
+Também tem as curvas de indutância de 0.5A até 128A, ao passo de uma corrente o dobro da anterior.
+Além disso, tem os daods de Ld e Lq em função da corrente *Ia* assim como a diferença entre eles e a razão de saliência.
+
+## Levantamento de Torques
+
+Nesta pasta está as curvas de torque extraídas do FEMM (Curvas_de_Torque_M-36).
+Dividindo essas curvas por suas respectivas médias foi obtido as curvas de torque oscilante (Curvas_de_Torque_ripple_M-36).
+Há as curvas de torque médio teórico, calculadas utilzando os valores de indutância da pasta **Levantamento das Indutâncias da Máquina**.
+E para as cuvas do torque médio simulado, foi efetuada a média das curvas de torque para múltiplos valores de corrente e combinados em um csv.
+
+## Levantamento dos vetores de campo magnético espaciais
+
+Essa pasta contém medições de campo no entreferro da máquina.
+Há curvas de fluxo e intensidade de campo para múltiplas correntes.
+Isso foi feito diretamente no FEMM, **ainda** não há script para essa metodologia.  
+Também há um arquivo com a medição do campo no entreferro para múltiplas correntes, medições em um único ponto do entreferro. 
+
+## Outros
+
+Esta pasta contém alguns dados extras, como as curvas do M-36  para corrente na BObina *Ia* e também para corrente trifásica.
+Também há alguns GIF's que ilustram o comportamento do rotor.
+
+## Tensão Induzida
+
+A curva de tensão induzida foi feita manualmente. Devido à metodologia do estudo, não foi possível utilizar a tensão induzida que o próprio FEMM fornece.
+Foram pegos os dados de fluxo concatenado e derivados de forma discretizada, ou seja, \frac{E[k]-E[k-1]}{$\delta$ t}. 
